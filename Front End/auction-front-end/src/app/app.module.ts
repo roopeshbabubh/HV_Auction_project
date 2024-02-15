c4d479a792e2cdacd234de6c8e10af7b35030e83
Roopesh <roopeshbabubh@gmail.com>
2024-02-15 21:12:17 +0530

    home-page

--- Front End/auction-front-end/src/app/app.module.ts
+++ Front End/auction-front-end/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CarouselComponent } from './home/components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
